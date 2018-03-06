7401865db75ccbb8bb24d7881aa6a4921838b92b
Zac <mrz7@live.com>
2018-03-05 22:38:47 -0700

    Initial commit

---
+++ src/com/zachary/gq/Main.java
@@ -0,0 +1,44 @@
+package com.zachary.gq;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //String Concatenation
+        //https://www.javatpoint.com/string-concatenation-in-java
+        //https://classroom.udacity.com/courses/ud282/lessons/8193672240/concepts/82409094050923#
+
+        int stops;
+        int fare;
+        stops = 0;
+        fare =0;
+        stops = stops +5;
+        fare = fare +2;
+
+        System.out.println("The bus made $"+fare+" after "+stops+" stops");
+
+        //http://www.openscreenshot.com/rkvAtz7df
+
+        String language = "java";
+        String feeling = "Love";
+        feeling = feeling.toLowerCase();
+        language = language.toUpperCase();
+        System.out.println("I " + feeling + " " + language);
+
+        // Camel Case, Lower is for variable names.
+        String driverFirstName;
+        driverFirstName = "Hamish";
+
+        //http://www.openscreenshot.com/HJJr5YXdf
+
+        //Lesson 2, Control Flow and Conditionals.
+
+        
+
+
+
+
+
+
+    }
+}
